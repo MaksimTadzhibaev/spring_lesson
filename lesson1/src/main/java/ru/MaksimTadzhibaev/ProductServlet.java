4f4792f34b5b3e256ba18c2a4167704fc8e7ec72
MaksimTadzhibaev <miromaxel@yandex.ru>
2021-06-24 22:33:05 +0300

    Выполнение ДЗ к 2 уроку часть 1

--- lesson1/src/main/java/ru/MaksimTadzhibaev/ProductServlet.java
+++ lesson1/src/main/java/ru/MaksimTadzhibaev/ProductServlet.java
@@ -9,9 +9,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
-@WebServlet(urlPatterns = "/product")
+@WebServlet(urlPatterns = "/products/*")
 public class ProductServlet extends HttpServlet {
 
     private ProductRepository productRepository;
@@ -24,14 +25,35 @@ public class ProductServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         List<Product> products = productRepository.findAll();
-        resp.getWriter().println("<table border=\"1\" width=\"600\" align=\"center\">");
-        resp.getWriter().println("<colgroup>");
-        resp.getWriter().println("<caption>Hello, products</caption>");
-        resp.getWriter().println("<tr align=\"center\"><th>id product</th><th>name product</th><th>cost product</th></tr>"); //ряд с ячейками заголовков
-        for (int i = 0; i < products.size(); i++) {
-            resp.getWriter().println("<tr align=\"center\"><td>" + products.get(i).getId() + "</td><td>" + products.get(i).getName() + "</td><td>" + products.get(i).getCost() + "</td></tr>");
-        } //ряд с ячейками тела таблицы
-        resp.getWriter().println("<colgroup>");
-        resp.getWriter().println("</table>");
+        PrintWriter wr = resp.getWriter();
+        if (req.getPathInfo() == null) {
+            //вывод информации обо всех продуктах
+            wr.println("<table border=\"1\" width=\"600\" align=\"center\">");
+            wr.println("<colgroup>");
+            wr.println("<caption>Hello, products</caption>");
+            wr.println("<tr align=\"center\"><th>id product</th><th>name product</th><th>cost product</th></tr>"); //ряд с ячейками заголовков
+            for (int i = 0; i < products.size(); i++) {
+                wr.println("<tr align=\"center\"><td>" + products.get(i).getId() + "</td><td>"
+                        + "<a href=\"http://194.67.93.77:8080/servlet-app/products/"
+                        + products.get(i).getId() + "\">" + products.get(i).getName()
+                        + "</a>" + "</td><td>" + products.get(i).getCost() + "</td></tr>");
+            } //ряд с ячейками тела таблицы
+            wr.println("<colgroup>");
+            wr.println("</table>");
+        } else {
+            //вывод информации о конкретном продукте (переход по ссылке);
+            String getProduct = req.getPathInfo().replace("/", "");
+            Long getNumProduct = Long.valueOf(getProduct) - 1L;
+            wr.println("<table border=\"1\" width=\"600\" align=\"center\">");
+            wr.println("<colgroup>");
+            wr.println("<caption>Hello, product info</caption>");
+            wr.println("<tr align=\"center\"><th>name</th><th>cost</th></tr>"); //ряд с ячейками заголовков
+            wr.println("<tr align=\"center\"><td>" + products.get((Math.toIntExact(getNumProduct))).getName()
+                    + "</td><td>" + products.get(Math.toIntExact(getNumProduct)).getCost()
+                    + "</td></tr>"); //ряд с ячейками тела таблицы
+            wr.println("<colgroup>");
+            wr.println("</table>");
+
+        }
     }
 }
